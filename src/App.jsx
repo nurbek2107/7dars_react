da2f94b689409f75b9d0e6d3c4057ce2d11c37df
nurbek2107 <abduaxatov002@gmail.com>
2024-06-05 15:30:07 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import MainLayout from "./layout/MainLayout"
 function App() {
-  return (
-    <div>App</div>
-  )
-}
+    const routes = createBrowserRouter([
+      {
+        path:"/",
+        element: <MainLayout/>
+      }
+    ])
+    return <RouterProvider router={routes} />
+} 
 
 export default App
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+
+import Navbar from "../components/header/Header"
+import Footer from "../components/footer/Footer"
+
+function MainLayout() {
+  return (
+    <>
+     <Navbar/>
+     <main>
+      <Outlet />
+      </main>
+     <Footer/>
+     
+    </>
+  );
+}
+
+export default MainLayout;
