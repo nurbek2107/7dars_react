d58aa0a28296b1ae858e44ec08a3b052ee316e93
nurbek2107 <abduaxatov002@gmail.com>
2024-06-06 16:11:51 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
-import MainLayout from "./layout/MainLayout"
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+
 function App() {
     const routes = createBrowserRouter([
       {
         path:"/",
-        element: <MainLayout/>
+        element: <MainLayout/>,
+        children:[{
+          index:true,
+          element:<Home/>
+        },
+        {
+          path:"/about",
+          element:<About/>
+        },
+        {
+          path:"/contact",
+          element:<Contact/>
+        }
+      ]
       }
     ])
     return <RouterProvider router={routes} />
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/header/Header"
 import Footer from "../components/footer/Footer"
 
+
 function MainLayout() {
   return (
     <>
